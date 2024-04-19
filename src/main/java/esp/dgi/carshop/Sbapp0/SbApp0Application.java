a146066898e8c5c2c55b198c2fced960fd6871b2
famaNar <famaamar@esp.sn>
2024-04-19 23:14:54 +0000

    Projet Spring

---
+++ src/main/java/esp/dgi/carshop/Sbapp0/SbApp0Application.java
@@ -0,0 +1,17 @@
+package esp.dgi.carshop.Sbapp0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp0Application.class, args);
+		System.out.println("Hello !my name is Fama");
+		System.out.println("I love SpringBoot");
+
+
+	}
+
+}
